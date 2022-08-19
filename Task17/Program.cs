c2bd07f1d4b23f6a4eee984491505b074f160f97
Natalli <MobileVersion.Nataliayandx1212@yandex.by>
2022-08-19 05:09:11 +0300

    add Task17

---
+++ Task17/Program.cs
@@ -0,0 +1,30 @@
+﻿// Написать программу, которая принимает на вход координаты (X, Y), X не = 0 и Y не = 0
+// и выдает номер четверти плоскости этой точки
+Console.WriteLine("введите коорд. т.А по Х");
+int x = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("введите коорд. т.А по Y");
+int y = Convert.ToInt32(Console.ReadLine());
+
+if (x > 0 && y > 0)
+{
+    Console.WriteLine("т.А в I четверти");
+}
+else if (x < 0 && y > 0)
+{
+    Console.WriteLine("т.А в II четверти");
+}
+else if (x < 0 && y < 0)
+{
+    Console.WriteLine("т.А в III четверти");
+}
+else if (x>0 && y < 0)
+{
+    Console.WriteLine("т.А в IV четверти");
+}
+else
+{
+    Console.WriteLine("четверть т.А определить невозможно");
+} 
+// & - логическое И &&- если хоть одно усл ложь вычислен прекращ
+//  | - ИЛИ || - или или - если хоть одно усл истина  то вычис прекращ.
